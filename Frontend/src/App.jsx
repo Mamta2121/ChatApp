f2f049d79c0c25ff27364075d5772a022e43023a
MamtaRajera <mamtarajera157@gmaill.com>
2024-12-13 13:13:23 +0530

    container config done

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Left from './home/left/Left'
+import Right from './home/right/right'
+
+
+function App() {
+  return (
+    <div>
+        <Left/>
+        <Right />
+    </div>
+  )
+}
+
+export default App
---
+++ Frontend/src/home/left/Left.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Left() {
+  return (
+    <div classname = ' w-[70%] bg-slate-950 text-white'>
+      LEFT
+    </div>
+  )
+}
+
+export default Left
