388a84514cc4d8dc6e102b1e72956da107f51217
MamtaRajera <mamtarajera157@gmaill.com>
2024-12-17 20:39:02 +0530

    container config done

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -1,6 +1,7 @@
 //Define all the functions here 
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import createTokenAndSaveCookie from  "../jwt/generateToken.js";
 
 export const signup = async (req, res) => {
 
@@ -27,10 +28,43 @@ export const signup = async (req, res) => {
       }
     );
   
-    await newUser.save().then( ()=> {
-      res.status(201).json( { message: "User Registered Successfully"});
-    });
+    await newUser.save();
+    
+    if(newUser)
+    {
+       createTokenAndSaveCookie(newUser._id, res);
+       res.status(201).json({message: "User Registered Successfully", newUser});
+    }  
   } catch (error) {
     console.log(error);
+    res.status(500).json({message: "Server Error"});
   }
+};
+
+export const login = async (req, res) => {
+    
+
+  try {
+
+    const {email, password} = req.body;
+    const user = await User.findOne({email});
+    if( !user)
+    {
+      return res.status(404).json({message:"Invalid User or password"});
+
+    };  
+    const isMatch = await bcrypt.compare(password, user.password);
+    if( !isMatch)
+    {
+       return res.status(404).json({message:"Invalid User or Password"}, user);
+    };
+
+    createTokenAndSaveCookie(user._id, res);
+    res.status(201).json( {message: "User Logged in Successfully", user})
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({message: "Server Error"});
+
+  } ;
 };
